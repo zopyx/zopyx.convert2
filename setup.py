84b36f18a4019b342170b88cef86fa9234b16ebf
Andreas Jung <yet@gmx.de>
2011-11-13 16:42:13 +0100

    updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
 ]
 
-version = '2.4.2'
+version = '2.4.2.1'
 
 desc = unicode(file(os.path.join('docs', 'source', 'README.rst')).read().strip(), 'utf-8')
 changes = file(os.path.join('docs', 'source', 'HISTORY.rst')).read().strip()
